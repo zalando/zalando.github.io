def824b1a51d45c27787f2eaa38e3bf8fa4d7b42
Per Ploug <per.ploug@zalando.de>
2018-04-20 17:31:53 +0200

    add in catwatch stats
    Loading catwatch stats and projects into the site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 //Catwatch urls
 var catwatch = {
-    projects: "https://catwatch.opensource.zalan.do/statistics?organizations=zalando",
-    statistics: "https://catwatch.opensource.zalan.do/projects?organizations=zalando",
+    statistics: "https://catwatch.opensource.zalan.do/statistics?organizations=zalando",
+    projects: "https://catwatch.opensource.zalan.do/projects?organizations=zalando",
 }
 
 // good ol callback, lets not make this too complicated
@@ -28,11 +28,21 @@ var getUrl = function(url, cb){
 
 //fetch the first 4 project
 getUrl(catwatch.projects + "&limit=4&sortBy=-stars", function(projects){
+    var template = document.getElementById("catwatch-projects-template");
+    var element = document.getElementById('catwatch-projects');
+
     console.log(projects);
+
+    var html =  Mustache.render(template.innerHTML, {projects: projects.splice(2)} );
+    html +=  Mustache.render(template.innerHTML, {projects: projects.splice(0,2)} );
+
+    element.innerHTML = html;
 });
 
 //fetch the stats
 getUrl(catwatch.statistics, function(stats){
-    console.log(stats);    
+    var template = document.getElementById("catwatch-statistics-template");
+    var element = document.getElementById('catwatch-statistics');
+    element.innerHTML = Mustache.render(template.innerHTML, stats[0]);
 });
 
