c7f5fd7c4030b8290935a949b0bd62c64e322097
psequeira <princiya.marina.sequeira@zalando.de>
2018-04-27 10:16:22 +0200

    favicon, other polishing
    Signed-off-by: psequeira <princiya.marina.sequeira@zalando.de>

--- assets/js/user.js
+++ assets/js/user.js
@@ -4,12 +4,13 @@ function user(data) {
       <a href="${data.html_url}" target="_blank">
         <div class="user__icon" style="background-image: url(${data.avatar_url})"></div>
       </a>
-      <h3 class="dc-h3">
+      <h3 class="dc-h2">
         <a href="${data.html_url}" target="_blank" class="user__name">
           ${data.name}
         </a>
       </h3>
-      <p class="dc-p">${data.title}</p>
+      <h3 class="dc-h3 user__title">${data.title}</h3>
+      <p class="dc-p">${data.bio || ''}</p>
     </div>
   `;
 }
