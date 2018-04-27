ffd342520d5e46205220c747f9a2c3061d4e590e
Per Ploug <per.ploug@zalando.de>
2018-04-27 10:34:32 +0200

    finetuning statics font and style
    Signed-off-by: Per Ploug <per.ploug@zalando.de>

--- assets/js/statistic.js
+++ assets/js/statistic.js
@@ -1,14 +1,14 @@
 function statistic(metric) {
   return `
-    <div class="dc-h3 statistic">
-      There are currently <span class="statistic__value">${metric.publicProjectCount}</span> active Zalando
-      <span class="statistic__key">projects</span>
-      written in <span class="statistic__value">${metric.programLanguagesCount}</span> different
-      <span class="statistic__key">languages</span>,
-      with <span class="statistic__value">${metric.allStarsCount}</span>
-      <span class="statistic__key">stars</span>
-      and <span class="statistic__value">${metric.allContributorsCount}</span> active
-      <span class="statistic__key">contributors</span>.
-    </div>
+  <p class="dc-p statistic">
+  There are currently <span class="statistic__value">${metric.publicProjectCount}</span> active Zalando
+  <span class="statistic__key">projects</span>
+  written in <span class="statistic__value">${metric.programLanguagesCount}</span> different
+  <span class="statistic__key">languages</span>,
+  with <span class="statistic__value">${metric.allStarsCount}</span>
+  <span class="statistic__key">stars</span>
+  and <span class="statistic__value">${metric.allContributorsCount}</span> active
+  <span class="statistic__key">contributors</span>.
+</p>
   `;
 }
