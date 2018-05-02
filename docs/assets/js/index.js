4a88f493bcf6e2164a81d841ac350ee2d985fef6
psequeira <princiya.marina.sequeira@zalando.de>
2018-05-02 16:48:06 +0200

    code cleanup
    Signed-off-by: psequeira <princiya.marina.sequeira@zalando.de>

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,3 +1,18 @@
+async function getData(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+async function getProjects(organisation, language) {
+  let url = `${api.projects}&offset=${store.offset}&organizations=${organisation}&limit=${store.limit}`;
+
+  if (language) {
+    url = `${url}&language=${language}`;
+  }
+
+  return await getData(url);
+}
+
 function render(id, data) {
   document.getElementById(id).innerHTML = data;
 }
@@ -12,32 +27,23 @@ function updateProjectsToDisplay(projects) {
   return projectsToDisplay;
 }
 
-function renderProjects(projects, doMapProjectToLanguage = false) {
+function renderProjects(projects, isMapProjectToLanguage = false, isShowAll = true) {
   let projectsToDisplay = new Set();
 
-  if (doMapProjectToLanguage) {
+  if (isShowAll) {
     projectsToDisplay = updateProjectsToDisplay(store.projects);
   }
 
   projects.forEach((repo, index) => {
     projectsToDisplay.add(project(repo, index));
 
-    if (doMapProjectToLanguage) {
+    if (isMapProjectToLanguage) {
       mapProjectToLanguage(repo);
       store.projects.add(repo);
     }
   });
 
   render('catwatch-projects', [...projectsToDisplay].join(''));
-}
-
-async function getData(url) {
-  const response = await fetch(url);
-  return await response.json();
-}
-
-async function getProjects(organisation) {
-  return await getData(`${api.projects}&offset=${store.offset}&organizations=${organisation}&limit=${store.limit}`)
 }
 
 function mapProjectToLanguage(repo) {
@@ -51,22 +57,34 @@ function mapProjectToLanguage(repo) {
   store.languages.set(repo.primaryLanguage, projectsPerLanguage);
 }
 
-async function displayProjects(doLoadMoreProjects = false) {
+async function displayProjects(language) {
     const organisation = store.organisations[0];
-    let projectsToDisplay;
 
-    if (!doLoadMoreProjects) {
+    if (language) {
       store.offset = 0;
-      projectsToDisplay = new Set();
     } else {
       store.offset += store.limit;
     }
 
-    const data = await getProjects(organisation);
-
-    renderProjects(data, true);
+    const data = await getProjects(organisation, language);
+    renderProjects(data, true, !language);
     displayLoadMoreProjects();
 };
+
+function filterByLanguage(language) {
+  const filterOption = language || document.getElementById('language-options').value;
+
+  if (filterOption === 'All') {
+    displayProjects();
+  } else {
+    if (store.offset + store.limit < store.totalProjects) {
+      displayProjects(filterOption);
+    } else {
+      const projects = store.languages.get(filterOption);
+      renderProjects(projects, false);
+    }
+  }
+}
 
 function displayLanguageOptions() {
   const labels = [];
@@ -78,17 +96,6 @@ function displayLanguageOptions() {
   render('language-options', labels.join(''));
 }
 
-function filterByLanguage(language) {
-  const filterOption = language || document.getElementById('language-options').value;
-
-  if (filterOption === 'All') {
-    displayProjects(true);
-  } else {
-    const projects = store.languages.get(filterOption);
-    renderProjects(projects, false);
-  }
-}
-
 function displayTopProgrammingLanguages() {
   const labels = [];
   for (const language of store.topProgrammingLanguages) {
@@ -98,7 +105,7 @@ function displayTopProgrammingLanguages() {
 }
 
 function displayLoadMoreProjects() {
-  if (store.offset < store.totalProjects) {
+  if (store.offset + store.limit < store.totalProjects) {
     render('load-more-projects-button', loadMoreProjects());
   } else {
     render('load-more-projects-button', '');
