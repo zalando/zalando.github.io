201acc0c5ffb5d0c6c0cc3b5395affa4e2f86248
psequeira <princiya.marina.sequeira@zalando.de>
2018-05-04 09:49:03 +0200

    broken projects link fixed
    Signed-off-by: psequeira <princiya.marina.sequeira@zalando.de>

--- docs/components/header.js
+++ docs/components/header.js
@@ -8,7 +8,7 @@ function header() {
           <img src="${path}assets/svg/logo.svg" />
         </a>
       </li>
-      <li class="dc-tab__element dc-tab__element--active"><a href="../index.html#os-projects">Projects</a></li>
+      <li class="dc-tab__element dc-tab__element--active"><a href="${path}index.html#os-projects">Projects</a></li>
       <li class="dc-tab__element"><a href="${path}index.html#os-goals">Purpose</a></li>
       <li class="dc-tab__element"><a href="${path}index.html#os-jobs">Jobs</a></li>
       <li class="dc-tab__element"><a href="${path}index.html#os-articles">Articles</a></li>
