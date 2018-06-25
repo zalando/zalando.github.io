74964199e82c296d90b4d11a056747e0687efb05
princiya <princiya.marina@gmail.com>
2018-06-25 17:13:34 +0200

    media queries for projects, fonts, header
    Signed-off-by: princiya <princiya.marina@gmail.com>

--- components/project.js
+++ components/project.js
@@ -9,34 +9,36 @@ function project(repo) {
     '';
 
   return `
-    <div class="project dc-card" style="${bgImage}">
-      <a href="${repo.url}" target="_blank">
-        <div class="project__header">
-          <div class="project__header-content">
-            <div class="project__header-content-text">
-              ${repo.name}
+    <div class="dc-column dc-column--large-4 dc-column--medium-12 dc-column--small-12">
+      <div class="project dc-card" style="${bgImage}">
+        <a href="${repo.url}" target="_blank">
+          <div class="project__header">
+            <div class="project__header-content">
+              <div class="project__header-content-text">
+                ${repo.name}
+              </div>
+            </div>
+            ${languageLabel}
+          </div>
+          <div class="project__content">
+            <p class="dc-p project__description">${repo.description}</p>
+            <div class="project__metrics">
+              <div class="project__metric" title="Stars">
+                <div class="project__metric-icon"><img src="${path}assets/svg/star.svg" /></div>
+                <div class="project__metric-value">${repo.starsCount}</div>
+              </div>
+              <div class="project__metric" title="Forks">
+                <div class="project__metric-icon"><img src="${path}assets/svg/fork.svg" /></div>
+                <div class="project__metric-value">${repo.forksCount}</div>
+              </div>
+              <div class="project__metric" title="Contributors">
+                <div class="project__metric-icon"><img src="${path}assets/svg/user.svg" /></div>
+                <div class="project__metric-value">${repo.contributorsCount}</div>
+              </div>
             </div>
           </div>
-          ${languageLabel}
-        </div>
-        <div class="project__content">
-          <p class="dc-p project__description">${repo.description}</p>
-          <div class="project__metrics">
-            <div class="project__metric" title="Stars">
-              <div class="project__metric-icon"><img src="${path}assets/svg/star.svg" /></div>
-              <div class="project__metric-value">${repo.starsCount}</div>
-            </div>
-            <div class="project__metric" title="Forks">
-              <div class="project__metric-icon"><img src="${path}assets/svg/fork.svg" /></div>
-              <div class="project__metric-value">${repo.forksCount}</div>
-            </div>
-            <div class="project__metric" title="Contributors">
-              <div class="project__metric-icon"><img src="${path}assets/svg/user.svg" /></div>
-              <div class="project__metric-value">${repo.contributorsCount}</div>
-            </div>
-          </div>
-        </div>
-      </a>
+        </a>
+      </div>
     </div>
   `;
 }
