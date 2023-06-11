376a0229ede0293ed718e4de03b7bafa78952d10
Bartosz Ocytko <bocytko@gmail.com>
2023-06-11 17:17:37 +0200

    adds alt text to a tags and replaces headings with divs
    Signed-off-by: Bartosz Ocytko <bocytko@gmail.com>

--- components/user.js
+++ components/user.js
@@ -1,15 +1,15 @@
 function user(data) {
   return `
     <div class="dc-column user">
-      <a href="${data.html_url}" target="_blank">
+      <a href="${data.html_url}" target="_blank" aria-label="View ${data.name}'s external profile">
         <div class="user__icon" style="background-image: url(${data.avatar_url})"></div>
       </a>
-      <h3 class="dc-h2">
+      <div class="dc-h2">
         <a href="${data.html_url}" target="_blank" class="user__name">
           ${data.name}
         </a>
-      </h3>
-      <h3 class="dc-h3 user__title">${data.title}</h3>
+      </div>
+      <div class="dc-h3 user__title">${data.title}</div>
       <p class="dc-p">${data.bio || ''}</p>
     </div>
   `;
