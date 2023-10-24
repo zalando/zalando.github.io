84a713d01a671f242453177502ee16a51ac47f51
Bartosz Ocytko <bocytko@gmail.com>
2023-10-24 11:39:12 +0200

    Hong left the team
    Signed-off-by: Bartosz Ocytko <bocytko@gmail.com>

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,14 +3,14 @@
  * Get the relevant fields from the output and add it to to below team array.
  */
 const team = [
-  {
-    username: "hpdang",
-    title: "Inner Source Manager",
-    name: "Hong Phuc Dang",
-    html_url: "https://github.com/hpdang",
+  /*{
+    username: "username",
+    title: "Title",
+    name: "Full Name",
+    html_url: "https://github.com/username",
     avatar_url:
       "https://avatars1.githubusercontent.com/u/1658742?s=400&u=47f83edbdae3ede5eba1aca10ea3eb378a74b8fd&v=4",
     bio:
-      "Inner source consultant | Open source specialist | Community leader | Mentor | "
-  }
+      "Role1 | Role 2"
+  }*/
 ];
